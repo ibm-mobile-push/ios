a04adc49670c60e516b1f6a2412343b41f052cff
Jeremy Buchman <buchmanj@us.ibm.com>
2019-01-09 08:39:40 -0700

    Updated config for Sample Apps

--- samples/ObjC Manual Dictionary Sample/SampleNotificationService/NotificationService.m
+++ samples/ObjC Manual Dictionary Sample/SampleNotificationService/NotificationService.m
@@ -8,8 +8,48 @@
  */
 
 #import "NotificationService.h"
+#import <IBMMobilePushNotificationService/IBMMobilePushNotificationService.h>
 
 @implementation NotificationService
+
+-(instancetype)init {
+    NSDictionary * config = @{
+                              @"baseUrl": @"https://api.ibm.xtify.com",
+                              @"appKey": @{
+                                      @"dev":  @"INSERT DEV APPKEY HERE",
+                                      @"prod": @"INSERT PROD APPKEY HERE"
+                                      },
+                              @"autoReinitialize": @YES,
+                              @"invalidateExistingUser": @NO,
+                              @"location": @{
+                                      @"autoInitialize": @YES,
+                                      @"sync": @{
+                                              @"syncRadius": @100000,
+                                              @"syncInterval": @300
+                                              },
+                                      @"geofence": @{
+                                              @"accuracy": @"3km"
+                                              },
+                                      @"ibeacon": @{
+                                              @"UUID": @"SET YOUR IBEACON UUID HERE"
+                                              }
+                                      },
+                              @"autoInitialize": @YES,
+                              @"sessionTimeout": @20,
+                              @"loglevel": @"verbose",
+                              @"logfile": @YES,
+                              @"watch": @{
+                                      @"category": @"mce-watch-category",
+                                      @"handoff": @{
+                                              @"userActivityName": @"com.mce.application",
+                                              @"interfaceController": @"handoff"
+                                              }
+                                      }
+                              };
+    
+    [MCEConfig sharedInstanceWithDictionary: config];
+    return [super init];
+}
 
 
 @end
